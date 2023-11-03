a1ec81cd01575b2ed1accc5ecabc571e4db49a1c
Lexsak <bartoszpiprek123@gmail.com>
2023-11-03 11:41:01 +0100

    Super Quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,11 +4,14 @@ const Button = ({ label, iconURL }) => {
         border font-mosserrat text-lg leading-none bg-coral-red rounded-full 
         text-white border-coral-red">
             {label}
-            <img
-                src={iconURL}
-                alt="arrow right icon"
-                className="ml-2 rounded-full w-5 h-5 "
-            />
+
+            {iconURL &&
+                <img
+                    src={iconURL}
+                    alt="arrow right icon"
+                    className="ml-2 rounded-full w-5 h-5 "
+                />
+            }
         </button>
     )
 }
