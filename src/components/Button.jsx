5a5de0d8a7f545ebeebf81fc0d0e7710c6de227e
Lexsak <bartoszpiprek123@gmail.com>
2023-11-03 16:49:40 +0100

    Subscribe Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
     return (
         <button className={`flex justify-center items-center gap-2 px-7 py-4 
         border font-mosserrat text-lg leading-none rounded-full transition-all hover:scale-90
         ${backgroundColor ?
                 `${backgroundColor} ${textColor} ${borderColor}` :
-                'bg-coral-red rounded-full text-white border-coral-red'}`}>
+                'bg-coral-red rounded-full text-white border-coral-red'}
+        ${fullWidth && 'w-full'}
+        `}>
             {label}
 
             {iconURL &&
